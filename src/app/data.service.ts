a15fab704ea9dde5db37aa2dd23ebc0e61d4dbdb
David Delarosa <xdavidel@gmail.com>
2018-08-05 16:36:12 +0300

    angular upgrade

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { map } from "rxjs/operators";
+
+import "rxjs/add/operator/map";
 
 @Injectable()
 export class DataService {
-
   result: any;
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
   getPrices() {
-    return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD")
-      .map(result => this.result = result);
+    return this._http
+      .get(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD"
+      )
+      .pipe(map(result => (this.result = result)));
   }
 }
